a168d92a994472d9020671fba7bf25d2ba6f7d3c
OlhaRyzha <104225380+OlhaRyzha@users.noreply.github.com>
2025-06-26 13:34:47 +0300

    updated saveAudioFile

--- src/utils/save-audio.ts
+++ src/utils/save-audio.ts
@@ -1,28 +1,14 @@
 import fs from 'fs/promises'
 import path from 'path'
 import config from '../config'
-import ffmpeg from 'fluent-ffmpeg'
-
-ffmpeg.setFfmpegPath('/opt/homebrew/bin/ffmpeg')
 
 export const saveAudioFile = async (id: string, filename: string, buffer: Buffer) => {
   const ext = path.extname(filename)
-  const tempPath = path.join(config.storage.uploadsDir, `${id}-temp${ext}`)
-  const outputName = `${id}.mp3`
+  const outputName = `${id}${ext}`
   const outputPath = path.join(config.storage.uploadsDir, outputName)
 
   await fs.mkdir(config.storage.uploadsDir, { recursive: true })
-  await fs.writeFile(tempPath, buffer)
+  await fs.writeFile(outputPath, buffer)
 
-  await new Promise<void>((resolve, reject) => {
-    ffmpeg(tempPath)
-      .audioBitrate(128)
-      .toFormat('mp3')
-      .save(outputPath)
-      .on('end', () => resolve())
-      .on('error', (err: unknown) => reject(err))
-  })
-
-  await fs.unlink(tempPath)
   return outputName
 }
